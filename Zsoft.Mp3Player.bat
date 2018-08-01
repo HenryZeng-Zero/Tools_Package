32e6e9a14088894ce5bc878cba0475a2a9a231a7
zzh <2879911298@qq.com>
2018-08-01 09:39:05 +0800

    up

---
+++ Zsoft.Mp3Player.bat
@@ -0,0 +1,3 @@
+@echo off
+wget.exe --no-check-certificate https://github.com/zzh-blog/pkg/raw/master/Zsoft.Mp3Player.7z
+7z.exe x Zsoft.Mp3Player.7z -y -aos
---
+++ Zsoft.Mp3Playeru.bat
@@ -0,0 +1,3 @@
+@echo off
+rd/s/q Zsoft.Mp3Player
+del /F /S /Q lnk\Zsoft.Mp3Player.exe
